739130399531fb0b0621e8a2007d0e91fd3bfe7b
Katsuaki Horiuchi <katsuaki.horiuchi@link-u.co.jp>
2025-03-25 19:04:41 +0900

    fix on module load error

--- Dockerfile
+++ Dockerfile
@@ -18,7 +18,7 @@ ENV GODEBUG="netdns=go http2server=0"
 
 RUN make build BUILD_VERSION=${BUILD_VERSION}
 
-FROM alpine:3.13.4
+FROM alpine:3.21.3
 LABEL maintainer="github.com/subspacecommunity/subspace"
 
 COPY --from=build  /src/subspace /usr/bin/subspace
--- entrypoint.sh
+++ entrypoint.sh
@@ -90,64 +90,64 @@ echo ${SUBSPACE_NAMESERVERS} | tr "," "\n" | while read -r ns; do echo "nameserv
 if [ -z "${SUBSPACE_DISABLE_MASQUERADE-}" ]; then
   if [[ ${SUBSPACE_IPV4_NAT_ENABLED} -ne 0 ]]; then
     # IPv4
-    if ! /sbin/iptables -t nat --check POSTROUTING -s ${SUBSPACE_IPV4_POOL} -j MASQUERADE; then
-      /sbin/iptables -t nat --append POSTROUTING -s ${SUBSPACE_IPV4_POOL} -j MASQUERADE
+    if ! /usr/sbin/iptables -t nat --check POSTROUTING -s ${SUBSPACE_IPV4_POOL} -j MASQUERADE; then
+      /usr/sbin/iptables -t nat --append POSTROUTING -s ${SUBSPACE_IPV4_POOL} -j MASQUERADE
     fi
 
-    if ! /sbin/iptables --check FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT; then
-      /sbin/iptables --append FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
+    if ! /usr/sbin/iptables --check FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT; then
+      /usr/sbin/iptables --append FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
     fi
 
-    if ! /sbin/iptables --check FORWARD -s ${SUBSPACE_IPV4_POOL} -j ACCEPT; then
-      /sbin/iptables --append FORWARD -s ${SUBSPACE_IPV4_POOL} -j ACCEPT
+    if ! /usr/sbin/iptables --check FORWARD -s ${SUBSPACE_IPV4_POOL} -j ACCEPT; then
+      /usr/sbin/iptables --append FORWARD -s ${SUBSPACE_IPV4_POOL} -j ACCEPT
     fi
   fi
 
   if [[ ${SUBSPACE_IPV6_NAT_ENABLED} -ne 0 ]]; then
     # IPv6
-    if ! /sbin/ip6tables -t nat --check POSTROUTING -s ${SUBSPACE_IPV6_POOL} -j MASQUERADE; then
-      /sbin/ip6tables -t nat --append POSTROUTING -s ${SUBSPACE_IPV6_POOL} -j MASQUERADE
+    if ! /usr/sbin/ip6tables -t nat --check POSTROUTING -s ${SUBSPACE_IPV6_POOL} -j MASQUERADE; then
+      /usr/sbin/ip6tables -t nat --append POSTROUTING -s ${SUBSPACE_IPV6_POOL} -j MASQUERADE
     fi
 
-    if ! /sbin/ip6tables --check FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT; then
-      /sbin/ip6tables --append FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
+    if ! /usr/sbin/ip6tables --check FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT; then
+      /usr/sbin/ip6tables --append FORWARD -m state --state RELATED,ESTABLISHED -j ACCEPT
     fi
 
-    if ! /sbin/ip6tables --check FORWARD -s ${SUBSPACE_IPV6_POOL} -j ACCEPT; then
-      /sbin/ip6tables --append FORWARD -s ${SUBSPACE_IPV6_POOL} -j ACCEPT
+    if ! /usr/sbin/ip6tables --check FORWARD -s ${SUBSPACE_IPV6_POOL} -j ACCEPT; then
+      /usr/sbin/ip6tables --append FORWARD -s ${SUBSPACE_IPV6_POOL} -j ACCEPT
     fi
   fi
 fi
 
 if [[ ${SUBSPACE_IPV4_NAT_ENABLED} -ne 0 ]]; then
   # ipv4 - DNS Leak Protection
-  if ! /sbin/iptables -t nat --check OUTPUT -s ${SUBSPACE_IPV4_POOL} -p udp --dport 53 -j DNAT --to ${SUBSPACE_IPV4_GW}:53; then
-    /sbin/iptables -t nat --append OUTPUT -s ${SUBSPACE_IPV4_POOL} -p udp --dport 53 -j DNAT --to ${SUBSPACE_IPV4_GW}:53
+  if ! /usr/sbin/iptables -t nat --check OUTPUT -s ${SUBSPACE_IPV4_POOL} -p udp --dport 53 -j DNAT --to ${SUBSPACE_IPV4_GW}:53; then
+    /usr/sbin/iptables -t nat --append OUTPUT -s ${SUBSPACE_IPV4_POOL} -p udp --dport 53 -j DNAT --to ${SUBSPACE_IPV4_GW}:53
   fi
 
-  if ! /sbin/iptables -t nat --check OUTPUT -s ${SUBSPACE_IPV4_POOL} -p tcp --dport 53 -j DNAT --to ${SUBSPACE_IPV4_GW}:53; then
-    /sbin/iptables -t nat --append OUTPUT -s ${SUBSPACE_IPV4_POOL} -p tcp --dport 53 -j DNAT --to ${SUBSPACE_IPV4_GW}:53
+  if ! /usr/sbin/iptables -t nat --check OUTPUT -s ${SUBSPACE_IPV4_POOL} -p tcp --dport 53 -j DNAT --to ${SUBSPACE_IPV4_GW}:53; then
+    /usr/sbin/iptables -t nat --append OUTPUT -s ${SUBSPACE_IPV4_POOL} -p tcp --dport 53 -j DNAT --to ${SUBSPACE_IPV4_GW}:53
   fi
 fi
 
 if [[ ${SUBSPACE_IPV6_NAT_ENABLED} -ne 0 ]]; then
   # ipv6 - DNS Leak Protection
-  if ! /sbin/ip6tables --wait -t nat --check OUTPUT -s ${SUBSPACE_IPV6_POOL} -p udp --dport 53 -j DNAT --to ${SUBSPACE_IPV6_GW}; then
-    /sbin/ip6tables --wait -t nat --append OUTPUT -s ${SUBSPACE_IPV6_POOL} -p udp --dport 53 -j DNAT --to ${SUBSPACE_IPV6_GW}
+  if ! /usr/sbin/ip6tables --wait -t nat --check OUTPUT -s ${SUBSPACE_IPV6_POOL} -p udp --dport 53 -j DNAT --to ${SUBSPACE_IPV6_GW}; then
+    /usr/sbin/ip6tables --wait -t nat --append OUTPUT -s ${SUBSPACE_IPV6_POOL} -p udp --dport 53 -j DNAT --to ${SUBSPACE_IPV6_GW}
   fi
 
-  if ! /sbin/ip6tables --wait -t nat --check OUTPUT -s ${SUBSPACE_IPV6_POOL} -p tcp --dport 53 -j DNAT --to ${SUBSPACE_IPV6_GW}; then
-    /sbin/ip6tables --wait -t nat --append OUTPUT -s ${SUBSPACE_IPV6_POOL} -p tcp --dport 53 -j DNAT --to ${SUBSPACE_IPV6_GW}
+  if ! /usr/sbin/ip6tables --wait -t nat --check OUTPUT -s ${SUBSPACE_IPV6_POOL} -p tcp --dport 53 -j DNAT --to ${SUBSPACE_IPV6_GW}; then
+    /usr/sbin/ip6tables --wait -t nat --append OUTPUT -s ${SUBSPACE_IPV6_POOL} -p tcp --dport 53 -j DNAT --to ${SUBSPACE_IPV6_GW}
   fi
 fi
 
 if [ ! -z ${SUBSPACE_LISTENPORT_ALTERNATE-} ]; then
-  if ! /sbin/iptables -t nat -C PREROUTING -p udp --dport ${SUBSPACE_LISTENPORT_ALTERNATE} -j REDIRECT --to-ports ${SUBSPACE_LISTENPORT}; then
-    /sbin/iptables -t nat -I PREROUTING -p udp --dport ${SUBSPACE_LISTENPORT_ALTERNATE} -j REDIRECT --to-ports ${SUBSPACE_LISTENPORT}
+  if ! /usr/sbin/iptables -t nat -C PREROUTING -p udp --dport ${SUBSPACE_LISTENPORT_ALTERNATE} -j REDIRECT --to-ports ${SUBSPACE_LISTENPORT}; then
+    /usr/sbin/iptables -t nat -I PREROUTING -p udp --dport ${SUBSPACE_LISTENPORT_ALTERNATE} -j REDIRECT --to-ports ${SUBSPACE_LISTENPORT}
   fi
 
-  if ! /sbin/ip6tables -t nat -C PREROUTING -p udp --dport ${SUBSPACE_LISTENPORT_ALTERNATE} -j REDIRECT --to-ports ${SUBSPACE_LISTENPORT}; then
-    /sbin/ip6tables -t nat -I PREROUTING -p udp --dport ${SUBSPACE_LISTENPORT_ALTERNATE} -j REDIRECT --to-ports ${SUBSPACE_LISTENPORT}
+  if ! /usr/sbin/ip6tables -t nat -C PREROUTING -p udp --dport ${SUBSPACE_LISTENPORT_ALTERNATE} -j REDIRECT --to-ports ${SUBSPACE_LISTENPORT}; then
+    /usr/sbin/ip6tables -t nat -I PREROUTING -p udp --dport ${SUBSPACE_LISTENPORT_ALTERNATE} -j REDIRECT --to-ports ${SUBSPACE_LISTENPORT}
   fi
 fi
 
